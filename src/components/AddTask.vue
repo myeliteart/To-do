461f5f66f5a9d90a04c2170e9afe65b28cd9bcfc
myeliteart <eliteny0@gmail.com>
2025-08-08 14:55:59 -0400

    update addTask.vue

--- src/components/AddTask.vue
+++ src/components/AddTask.vue
@@ -1,7 +1,7 @@
 <template>
-     <div class="rounded-lg border border-gray-400 p-3">
-             <input type="text" v-model.trim="store.text.txt" class="text-gray-600 font-bold focus:outline-none" placeholder="New Task"> <br>
-             <textarea placeholder="Description" v-model.trim="store.text.dsc" class="text-gray-600 focus:outline-none text-sm"></textarea> <br>
+     <div class="rounded-lg border border-gray-400 p-3 w-full">
+             <input type="text" v-model.trim="store.text.txt" class="text-gray-600 font-bold focus:outline-none w-[90%] sm:max-w-[100%] lg:max-w-[100%] flex-grow" placeholder="New Task"> <br>
+             <textarea placeholder="Description" v-model.trim="store.text.dsc" class="text-gray-600 focus:outline-none text-sm w-[90%] sm:max-w-[100%] lg:max-w-[100%] flex-grow"></textarea> <br>
           <div class="flex sm:flex-row flex-col sm:justify-between sm:items-center">
             <div>
                 <select v-model="store.text.selectedPriority" class="mr-9 focus:outline-none p-0 rounded-md cursor-pointer text-gray-600">
